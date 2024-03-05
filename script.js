d64c645eec5f9dc289b2b17d5896577c4b97887e
Ajeet Kumar <98801990+16ajeet@users.noreply.github.com>
2024-03-05 09:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	const form1 = document.getElementById("form1");
-	const valForm1 = form1.value;
-	alert("valForm1")
+	const fname = form1.elements["fname"].value;
+	const lname = form1.elements["lname"].value;
+	alert(`${fname} ${lname}`)
 
 }
